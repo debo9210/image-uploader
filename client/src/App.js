b326b1aedecaac9996be89463d5e5dbd630d23db
debo9210 <omololuadebowale1@gmail.com>
2020-12-30 06:53:55 -0500

    Added footer component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import ImageUploader from './components/ImageUploader';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className='App'>
       <ImageUploader />
+      <Footer />
     </div>
   );
 }
