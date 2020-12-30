c6370f443e37d120efb014e8969cb824693bf195
debo9210 <omololuadebowale1@gmail.com>
2020-12-30 05:40:55 -0500

    prepared app for deployment

--- server.js
+++ server.js
@@ -19,5 +19,15 @@ mongoose
 app.get('/', (req, res) => res.send('Welcome here!!!'));
 app.use('/api/images', imageUpload);
 
+//server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running @ port ${port}`));
